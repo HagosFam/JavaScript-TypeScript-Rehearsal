36a2399fd7ffd1e971517b8dc85d531c8d821bc0
hagos <111482114+Kytsync@users.noreply.github.com>
2023-06-06 19:02:44 -0500

    scope, global, function and block has been added

--- JavaScript/Simple JS/scope.js
+++ JavaScript/Simple JS/scope.js
@@ -2,6 +2,21 @@
 
 // Block scope
 
-// function scope
+// variable can not be accessed outside the given scope
+if(true) {
+    let name = 'Nigus';
+    console.log("name", name);
+}
 
-// global scope 
+// function scope 
+    // dectates that variables defind inside a function are not accessible outside of the function 
+    function add(a, b) {
+        const name = "Someone"; // this has function scope 
+        
+    }
+
+// global scope  
+   // the scope which is accessible everywhere in the js file
+   // global variable declared doesnt override the scope or function variable, the scope or function variable always wins 
+   const name = "Age"; // this has global scope and doesn't override the above names, 
+                       // if used outside of the above scope and function, this wins 
